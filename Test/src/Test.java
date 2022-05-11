e89415ec5121516e67a7b56d8e7f808ea10334d8
JeongSeonggil <dataofsg02@gmail.com>
2022-05-11 12:14:13 +0900

    Commit in Test2

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("print");
-        System.out.println("checkout Test");
+        System.out.println("checkout Test2");
     }
 }
