4a9dba15eaf471bde555ef11e0c5e5d17a50c6fa
SeongGil-Jeong <dataofsg02@gmail.com>
2022-06-20 23:00:22 +0900

    [#11] feature : sout 11

--- Test/src/Test62.java
+++ Test/src/Test62.java
@@ -1,23 +1,6 @@
 public class Test62 {
     public static void main(String[] args) {
-        System.out.println("Test62"); // Reset
-        System.out.println("Test10");
-        System.out.println("Test11");
-        System.out.println("GitFlow Start!");
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("test33");
-        System.out.println("GitFlow Start");
-        System.out.println("UpdateUser");
-        System.out.println("AddUser");
-        System.out.println("Add Item");
-        System.out.println("AddCat");
-        System.out.println("AddCat22");
-        System.out.println("AddDogggggggg");
-        System.out.println("Test3");
-        System.out.println("Test33 End");
-        System.out.println("Test 32 End");
-        System.out.println("Test 35 End");
+        System.out.println("#11");
     }
 
 }
